1ba3c548e7d2b812de775a030d5c3a89188f5818
Jamie Goodnight <goodnightlambda@gmail.com>
2019-02-11 14:45:46 -0500

    Initial commit. HeaderContainer set up. Hopefully.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,18 @@
 import React from 'react';
+
+import ImageThumbnail from "./HeaderComponents/ImageThumbnail";
+import HeaderTitle from "./HeaderComponents/HeaderTitle.js";
+import HeaderContent from "./HeaderComponents/Content.js";
 import './Header.css';
+
+function HeaderContainer(){
+    return (
+        <div>   
+            <ImageThumbnail />
+            <HeaderTitle />
+            <HeaderContent />
+        </div>
+    );
+}
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle(){
+    return (
+        <div>
+            <h1>Lambda School</h1>
+            <p>@LambdaSchool · 26 jan</p>
+        </div>
+    );
+}
+
+export default HeaderTitle;
