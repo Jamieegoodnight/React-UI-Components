07354f822b285ab23375d276da8b7ebdee01a0a0
Jamie Goodnight <goodnightlambda@gmail.com>
2019-02-11 14:58:27 -0500

    CardContainer components built. Hopefully.

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,16 @@
 import React from 'react';
+
+import CardBanner from "./CardComponents/CardBanner.js";
+import CardContent from "./CardComponents/CardContent.js";
 import './Card.css';
+
+function CardContainer(){
+    return (
+        <div>
+            <CardBanner />
+            <CardContent />
+        </div>
+    );
+}
+
+export default CardContainer;
